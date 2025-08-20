943877038c6d3b02e505a9e712f9d5d5334ca3be
klpFalros05 <franlucini05@gmail.com>
2025-08-19 23:14:23 -0300

    Inicial + .gitignore (excluir .vs/bin/obj)

--- Solution1/Ejercicio02/obj/Debug/net8.0/Ejercicio2.AssemblyInfo.cs
+++ Solution1/Ejercicio02/obj/Debug/net8.0/Ejercicio2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e79c1e8001c519cd8cdef955bb549373e1f45f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
